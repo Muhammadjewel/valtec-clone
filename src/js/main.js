c3e97270aa123b1b34ab081093ecfdd64081ba04
muhammadjewel <msuratov@gmail.com>
2020-06-02 09:29:09 +0500

    Add opening and closing buttons for sitenav__menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
+  // SITE-HEADER
   $('.site-header__catalog-toggler').on('click', function () {
     $('.site-header').toggleClass('site-header--with-catalog');
     $('.site-header__catalog').toggleClass('site-header__catalog--open');
+  });
+
+  // SITENAV
+  // Sitenav menu
+  var elSitenav = $('.sitenav__menu');
+  
+  $('.sitenav__menu-toggler').on('click', function () {
+    elSitenav.toggleClass('sitenav__menu--open');
+  });
+
+  $('.sitenav__menu-close').on('click', function () {
+    elSitenav.removeClass('sitenav__menu--open');
   });
 });
